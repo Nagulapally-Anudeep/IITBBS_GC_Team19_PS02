b8b17efe16b93c5d13735760a7a0ca2e435c20bc
Sathiya Seelan <codewithsathya@gmail.com>
2022-03-19 22:04:41 +0530

    changed .gitignore

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const userRouter = require("./routes/userRoutes");
 const questionRouter = require("./routes/questionRoutes");
 
 const app = express();
+const session = require("express-session");
+app.set("view engine", "ejs");
+app.use(session({
+	resave: false,
+	saveUninitialized: true,
+	secret: "secret"
+}));
+
+app.get("/", (req, res) => {
+	
+});
 
 app.use(express.static(`${__dirname}/public`));
 
