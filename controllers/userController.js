d2e9773ee9d9613a07f64eeb54f44d5eddb6b86b
Sathiya Seelan <codewithsathya@gmail.com>
2022-03-20 00:30:03 +0530

    added few routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,20 @@
 const User = require("./../models/userModel");
+const Question = require("./../models/questionModel");
+const Answer = require("../models/answerModel");
 
-exports.blackListUser = async (req, res, next) => {};
+exports.blackListUser = async (req, res, next) => {
+	
+};
 
 exports.whiteListUser = async (req, res, next) => {};
 
-exports.getUser = async (req, res, next) => {};
+exports.getUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  const userQuestions = await Question.find({ createdBy: user._id });
+  const userAnswers = await Answer.find({ createdBy: user._id });
+  res.render("user", {
+    user,
+    userQuestions,
+    userAnswers,
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
+const { ensureAuthenticated } = require("../middleware");
 
-router.route('/:id').get(userController.getUser);
-router.route('/:id').get(userController.whiteListUser);
-router.route('/:id').get(userController.blackListUser);
+router.route('/:id').get(ensureAuthenticated, userController.getUser);
+router.route('/:id').post(ensureAuthenticated, userController.whiteListUser);
+router.route('/:id').post(ensureAuthenticated, userController.blackListUser);
 
 module.exports = router;
